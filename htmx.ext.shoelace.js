2e660ec2bc4ac917eda683b515d6a0400d2b4c47
benopotamus <benopotamus@tinku.net>
2024-02-25 18:24:14 +1000

    Improvements based on @karolskolasinski 's PR

--- htmx.ext.shoelace.js
+++ htmx.ext.shoelace.js
@@ -3,30 +3,34 @@ const slTypes = 'sl-button, sl-checkbox, sl-color-picker, sl-input, sl-radio-gro
 /* Lightly modified version of the same function in htmx.js */
 function shouldInclude(elt) {
 
-	// sl-rating doesn't have a name attribute exposed through the Shoelace API
-	if (elt.tagName == 'SL-RATING' && elt.getAttribute('name')) {
-		return true;
+	// sl-rating doesn't have a name attribute exposed through the Shoelace API so this check needs to come before the name==="" check
+	if (elt.tagName === 'SL-RATING' && elt.getAttribute('name')) {
+		return true
 	}
 	if (elt.name === "" || elt.name == null || elt.disabled) {
-		return false;
+		return false
 	}
 	if (elt.type === "submit") {
-		return false;
+		return false
 	}
 	if (elt.tagName === 'SL-CHECKBOX' || elt.tagName === 'SL-SWITCH') {
-		return elt.checked;
+		return elt.checked
+	}
+	if (elt.tagName === "SL-RADIO-GROUP") {
+		return elt.value.length > 0
 	}
 	return true;
 }
 
 htmx.defineExtension('shoelace', {
 	onEvent : function(name, evt) {
-		if ((name === "htmx:configRequest") && (evt.detail.elt.tagName == 'FORM')) {
+		if ((name === "htmx:configRequest") && (evt.detail.elt.tagName === 'FORM')) {
 			evt.detail.elt.querySelectorAll(slTypes).forEach((elt) => {
 				if (shouldInclude(elt)) {
 
 					if (elt.tagName === 'SL-CHECKBOX' || elt.tagName === 'SL-SWITCH') {
-						evt.detail.parameters[elt.name] = elt.checked
+						// Temp Shoelace (unconfirmed) bug fix
+						evt.detail.parameters[elt.name] = elt.value || 'on'
 
 					} else if (elt.tagName == 'SL-RATING') {
 						evt.detail.parameters[elt.getAttribute('name')] = elt.value
