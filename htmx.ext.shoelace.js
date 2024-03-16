2941ed8f6c01b7162571577c19bc57524f35e047
benopotamus <benopotamus@tinku.net>
2024-03-16 13:16:20 +1000

    Added some comments. Removed ability for the extension to submit button values. I'll create an issue to add it back in a better way

--- htmx.ext.shoelace.js
+++ htmx.ext.shoelace.js
@@ -1,23 +1,25 @@
-const slTypes = 'sl-button, sl-checkbox, sl-color-picker, sl-input, sl-radio-group, sl-range, sl-rating, sl-select, sl-switch, sl-textarea'
+const slTypes = 'sl-checkbox, sl-color-picker, sl-input, sl-radio-group, sl-range, sl-rating, sl-select, sl-switch, sl-textarea'
 
 /* Lightly modified version of the same function in htmx.js */
 function shouldInclude(elt) {
-	// sl-rating doesn't have a name attribute exposed through the Shoelace API so this check needs to come before the name==="" check
+
+	// sl-rating doesn't have a name attribute exposed through the Shoelace API (for elt.name) so this check needs to come before the name==="" check
 	if (elt.tagName === 'SL-RATING' && elt.getAttribute('name')) {
 		return true
 	}
+
 	if (elt.name === "" || elt.name == null || elt.disabled || elt.closest("fieldset[disabled]")) {
 		return false
 	}
-	if (elt.type === "button" || elt.type === "submit" || elt.tagName === "image" || elt.tagName === "reset" || elt.tagName === "file" )  {
-		return false
-	}
+
 	if (elt.tagName === 'SL-CHECKBOX' || elt.tagName === 'SL-SWITCH') {
 		return elt.checked
 	}
+
 	if (elt.tagName === "SL-RADIO-GROUP") {
 		return elt.value.length > 0
 	}
+
 	return true;
 }
 
@@ -27,7 +29,7 @@ htmx.defineExtension('shoelace', {
 			evt.detail.elt.querySelectorAll(slTypes).forEach((elt) => {
 				if (shouldInclude(elt)) {
 					if (elt.tagName === 'SL-CHECKBOX' || elt.tagName === 'SL-SWITCH') {
-						// Temp Shoelace (unconfirmed) bug fix
+						// Shoelace normally does this bit internally when the formdata event fires, but htmx doesn't fire the formdata event, so we do it here instead. See https://github.com/shoelace-style/shoelace/issues/1891
 						evt.detail.parameters[elt.name] = elt.value || 'on'
 
 					} else if (elt.tagName == 'SL-RATING') {
