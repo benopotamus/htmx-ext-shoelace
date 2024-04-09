9b40155b46ac197ef47324f4182ea5a14e21239b
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-04-09 15:41:29 +0200

    Implement support for hx-include parameter

--- htmx.ext.shoelace.js
+++ htmx.ext.shoelace.js
@@ -23,10 +23,33 @@ function shouldInclude(elt) {
 	return true;
 }
 
+function queryElements(source) {
+	let elements = [];
+	if (source.tagName === 'FORM') {
+		elements.concat(Array.from(source.querySelectorAll(slTypes)));
+	}
+	if (source.hasAttribute("hx-include")) {
+		const includedSources = source.getAttribute("hx-include").split(",");
+		// Iterate through each form ID
+		includedSources.forEach(includedSource => {
+			// Get all input elements within the form using querySelectorAll
+			const includedSourceDOM = document.querySelector(includedSource);
+			if (includedSourceDOM !== null) {
+				const includedSlInputs = includedSourceDOM.querySelectorAll(slTypes);
+
+				// Convert the NodeList to an array and concatenate it to the allInputs array
+				elements = elements.concat(Array.from(includedSlInputs));
+			}
+		});
+	}
+	return elements;
+}
+
 htmx.defineExtension('shoelace', {
 	onEvent : function(name, evt) {
-		if ((name === "htmx:configRequest") && (evt.detail.elt.tagName === 'FORM')) {
-			evt.detail.elt.querySelectorAll(slTypes).forEach((elt) => {
+		if (name === "htmx:configRequest") {
+			const elements = queryElements(evt.detail.elt);
+			elements.forEach((elt) => {
 				if (shouldInclude(elt)) {
 					if (elt.tagName === 'SL-CHECKBOX' || elt.tagName === 'SL-SWITCH') {
 						// Shoelace normally does this bit internally when the formdata event fires, but htmx doesn't fire the formdata event, so we do it here instead. See https://github.com/shoelace-style/shoelace/issues/1891
