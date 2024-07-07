d5cf464b4f68a2a3c24b62e63431a61c00da4173
benopotamus <benopotamus@tinku.net>
2024-07-07 18:05:21 +1000

    Little tests and comments and things

--- htmx.ext.shoelace.js
+++ htmx.ext.shoelace.js
@@ -1,3 +1,4 @@
+// htmx 2.x now uses the formdata event - which Shoelace fires - so only these 2 Shoelace components need to be specially handled now
 const slTypes = 'sl-radio-group, sl-rating'
 
 /* Lightly modified version of the same function in htmx.js */
@@ -8,10 +9,12 @@ function shouldInclude(elt) {
 		return true
 	}
 
+	// Don't include disabled fields (or fields in disabled fieldsets) or fields without names
 	if (elt.name === '' || elt.name == null || elt.disabled || elt.closest('fieldset[disabled]')) {
 		return false
 	}
 
+	// Don't include sl-radio-group value if no option is selected - see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio#data_representation_of_a_radio_group
 	if (elt.tagName === 'SL-RADIO-GROUP') {
 		return elt.value.length > 0
 	}
